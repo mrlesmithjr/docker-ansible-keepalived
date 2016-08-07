a09bb67465680109b9590bf4978938cb2641a9ca
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-08-07 02:42:36 +0000

    Added environment vars for creating config
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ansible-playbook -i "localhost," -c local /docker-entrypoint.yml
+ansible-playbook -i "localhost," -c local /docker-entrypoint.yml --extra-vars "router_id=${ROUTER_ID} router_pri=${ROUTER_PRI} state=${STATE} vip_int=${VIP_INT} vip_address=${VIP_ADDRESS}"
 
-sleep 10
 /usr/sbin/keepalived -f /etc/keepalived/keepalived.conf --dont-fork --log-console
 #exec "$@"
